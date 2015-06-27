0275193c8835b5db432f0b84e5b1b950decd8cd5
brnbrns <brianburns48@gmail.com>
2015-06-27 13:25:02 -0700

    Added Javadoc comments to Vertex

--- src/Vertex.java
+++ src/Vertex.java
@@ -4,17 +4,39 @@
  * Contains the value and position on the board of a vertex (letter)
  ***************/
 
+/**
+ * Stores a character as a vertex on the board
+ * @author Brian Burns
+ */
 public class Vertex {
-	// Needed variables
+	/**
+   * The value of the character
+   */
 	private char value;
-	private int position;
+	/**
+   * The vertex's position on the board
+   */
+  private int position;
 	
-	// Constructor
+	/**
+   * Initializes the vertex
+   * @param v The character to store in the vertex
+   * @param p The vertex's position on the board
+   */
 	public Vertex(char v, int p) {
 		value = v;
 		position = p;
 	}
-	// Getters
+	
+  /**
+   * Returns the character stored in this vertex
+   * @return The character stored in this vertex
+   */
 	public char getChar() {return value;}
+
+  /**
+   * Returns this vertex's position on the board
+   * @return This vertex's position on the board
+   */
 	public int getPos() {return position;}
 }
